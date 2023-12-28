38d60b57a52685c37ffdd82ce9c89a39b34a1f3c
Thanh_01203 <ducthanh1122003@gmail.com>
2023-12-29 00:42:48 +0700

    vue deploy file

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/game_prroject/'
-  : '/'
+    ? '/game_prroject/'
+    : '/'
 })
